2dc4c0499a5dc18f25151492ac1ed5eecc83cd2b
Disha R <dishanjali3@gmail.com>
2026-08-20 00:11:43 +0530

    Add first three LeetCode solutions

---
+++ Arrays/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+
+        BestTimeToBuyAndSellStock obj =
+                new BestTimeToBuyAndSellStock();
+
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        int result = obj.maxProfit(prices);
+
+        System.out.println("Maximum Profit: " + result);
+    }
+}
