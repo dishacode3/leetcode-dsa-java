ae026527ccd87bc91d3db0b7f43bdba3fef0fa87
Disha R <dishanjali3@gmail.com>
2026-08-23 22:22:12 +0530

    Solve Running Sum of 1D Array

---
+++ Arrays/RunningSum.java
@@ -0,0 +1,25 @@
+package Arrays;
+public class RunningSum {
+
+    public static int[] runningSum(int[] nums) {
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = runningSum(nums);
+
+        System.out.print("Running Sum: ");
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
