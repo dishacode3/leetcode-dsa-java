90addfe4859c0971694b12a06a061047a69b53aa
Disha R <dishanjali3@gmail.com>
2026-08-30 22:09:49 +0530

    Solve Reverse String

---
+++ Strings/ReverseString.java
@@ -0,0 +1,38 @@
+package Strings;
+public class ReverseString {
+
+    public static void reverseString(char[] s) {
+
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+
+        System.out.println("Before reversing:");
+
+        for (char c : s) {
+            System.out.print(c + " ");
+        }
+
+        reverseString(s);
+
+        System.out.println("\nAfter reversing:");
+
+        for (char c : s) {
+            System.out.print(c + " ");
+        }
+    }
+}
