519fae112423933fd56f3d9c373485d899e444ba
Disha R <dishanjali3@gmail.com>
2026-08-30 23:01:41 +0530

    Solve Isomorphic String

---
+++ Strings/IsomorphicStrings.java
@@ -0,0 +1,45 @@
+package Strings;
+
+import java.util.HashMap;
+
+public class IsomorphicStrings {
+
+    public static boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (mapST.containsKey(c1) && mapST.get(c1) != c2) {
+                return false;
+            }
+
+            if (mapTS.containsKey(c2) && mapTS.get(c2) != c1) {
+                return false;
+            }
+
+            mapST.put(c1, c2);
+            mapTS.put(c2, c1);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "egg";
+        String t = "add";
+
+        System.out.println("String 1: " + s);
+        System.out.println("String 2: " + t);
+        System.out.println("Isomorphic: " + isIsomorphic(s, t));
+    }
+}
