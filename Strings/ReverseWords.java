986fb99a7f88a1eaa7c5170da34907ae6526a7d8
Disha R <dishanjali3@gmail.com>
2026-09-05 22:54:40 +0530

    Solve Reverse Words

---
+++ Strings/ReverseWords.java
@@ -0,0 +1,31 @@
+package Strings;
+public class ReverseWords {
+
+    public static String reverseWords(String s) {
+
+        String[] words = s.trim().split("\\s+");
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+
+            result.append(words[i]);
+
+            if (i != 0) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String s = "the sky is blue";
+
+        String result = reverseWords(s);
+
+        System.out.println("Input: " + s);
+        System.out.println("Output: " + result);
+    }
+}
